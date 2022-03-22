54bb7a677d17729f6865bd4ed4b46e9452bac6c7
kendlc <kennethcdlc@gmail.com>
2022-03-23 09:25:37 +1100

    progress 4: game done - fixing game visuals and initiation - adding features

--- js/logic.js
+++ js/logic.js
@@ -1,14 +1,15 @@
 
 
-const p1 = true
-const p2 = false
+
 let playerTurn = true
-let button = ''
+
 
 const cells = {
     c1:'',c2:'',c3:'',
     c4:'',c5:'',c6:'',
-    c7:'',c8:'',c9:'',
+    c7:'',c8:'',c9:'', 
+    moves : 0,
+    button: 0,
     checkCells: function (){
         if ((this.c1 && this.c5 && this.c9) ||
             (this.c3 && this.c5 && this.c7) ||
@@ -18,10 +19,9 @@ const cells = {
             (this.c7 && this.c4 && this.c1) ||
             (this.c2 && this.c5 && this.c8) ||
             (this.c4 && this.c5 && this.c6)){
-            console.log('player 1 wins')
+            $('#prompter').text(`Congratulations ${$('#player1').text()}!`);
             $('.block').addClass('unclickable');
-        }
-
+        } else 
         if ((this.c1 === false && this.c5 === false && this.c9 === false) ||
             (this.c3 === false && this.c5 === false && this.c7 === false) ||
             (this.c1 === false && this.c2 === false && this.c3 === false) ||
@@ -30,7 +30,11 @@ const cells = {
             (this.c7 === false && this.c4 === false && this.c1 === false) ||
             (this.c2 === false && this.c5 === false && this.c8 === false) ||
             (this.c4 === false && this.c5 === false && this.c6 === false)){
-            console.log('player 2 wins')
+            $('#prompter').text(`Congratulations ${$('#player2').text()}!`);
+            $('.block').addClass('unclickable');
+        } else
+        if (this.moves === 8) {
+            $('#prompter').text(`Draw!`);
             $('.block').addClass('unclickable');
         }
     },
@@ -40,73 +44,40 @@ const cells = {
         this.c7 = ''; this.c8 = ''; this.c9 = '';
         $('.block').removeClass('unclickable');
         $('.block').empty();
+        this.moves = 0;
     },
     player1Turn : function(){
-        $('<img src="http://www.fillmurray.com/150/150">').appendTo(`#cell${button}`);
-        $(`#cell${button}`).addClass('unclickable');
+        $('<img src="img/X.png" width="140">').appendTo(`#cell${this.button}`);
+        $(`#cell${this.button}`).addClass('unclickable');
+        $('#prompter').text(`It's ${$('#player2').text()}'s turn!`)
         cells.checkCells();
         playerTurn = false;
     },
     player2Turn : function(){
-        $('<img src="http://www.placebear.com/150/150">').appendTo(`#cell${button}`);
-        $(`#cell${button}`).addClass('unclickable');
+        $('<img src="img/O.png" width="140">').appendTo(`#cell${this.button}`);
+        $(`#cell${this.button}`).addClass('unclickable');
+        $('#prompter').text(`It's ${$('#player1').text()}'s turn!`);
         cells.checkCells();
         playerTurn = true;
     },
     playerInput : function(){
         if (playerTurn) {
-            cells[`c${button}`] = p1;
+            cells[`c${this.button}`] = true;
             cells.player1Turn();
+            this.moves += 1;
         }  
          else {
-            cells[`c${button}`] = p2;
+            cells[`c${this.button}`] = false;
             cells.player2Turn();
+            this.moves += 1;
         }
     }
+    
 }
 
 
 
-$('#cell1').on('click', function(){
-    button = 1
-    cells.playerInput();
-});
 
-$('#cell2').on('click', function(){
-    button = 2
-    cells.playerInput();
-});
-
-$('#cell3').on('click', function(){
-    button = 3
-    cells.playerInput();
-});
-
-$('#cell4').on('click', function(){
-    button = 4
-    cells.playerInput();
-});
-$('#cell5').on('click', function(){
-    button = 5
-    cells.playerInput();
-});
-$('#cell6').on('click', function(){
-    button = 6
-    cells.playerInput();
-});
-$('#cell7').on('click', function(){
-    button = 7
-    cells.playerInput();
-});
-
-$('#cell8').on('click', function(){
-    button = 8
-    cells.playerInput();
-});
-$('#cell9').on('click', function(){
-    button = 9
-    cells.playerInput();
-});
 
 
 
---
+++ js/object.js
@@ -0,0 +1,40 @@
+$('#cell1').on('click', function(){
+    cells.button = 1
+    cells.playerInput();
+});
+
+$('#cell2').on('click', function(){
+    cells.button = 2
+    cells.playerInput();
+});
+
+$('#cell3').on('click', function(){
+    cells.button = 3
+    cells.playerInput();
+});
+
+$('#cell4').on('click', function(){
+    cells.button = 4
+    cells.playerInput();
+});
+$('#cell5').on('click', function(){
+    cells.button = 5
+    cells.playerInput();
+});
+$('#cell6').on('click', function(){
+    cells.button = 6
+    cells.playerInput();
+});
+$('#cell7').on('click', function(){
+    cells.button = 7
+    cells.playerInput();
+});
+
+$('#cell8').on('click', function(){
+    cells.button = 8
+    cells.playerInput();
+});
+$('#cell9').on('click', function(){
+    cells.button = 9
+    cells.playerInput();
+});
